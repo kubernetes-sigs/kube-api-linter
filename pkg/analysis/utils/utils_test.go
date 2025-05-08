c17423e645a8513f824a4d35f40a1eae1518a639
sivchari <shibuuuu5@gmail.com>
2025-05-08 23:24:19 +0900

    use ginkgo
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

--- pkg/analysis/utils/utils_test.go
+++ pkg/analysis/utils/utils_test.go
@@ -1,20 +1,39 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package utils_test
 
 import (
 	"go/ast"
-	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 
 	"sigs.k8s.io/kube-api-linter/pkg/analysis/utils"
 )
 
-func TestFieldName(t *testing.T) {
-	t.Parallel()
-
-	cases := map[string]struct {
+var _ = Describe("FieldName", func() {
+	type fieldNameInput struct {
 		field *ast.Field
 		want  string
-	}{
-		"field has Names": {
+	}
+
+	DescribeTable("Should extract the field name", func(in fieldNameInput) {
+		Expect(utils.FieldName(in.field)).To(Equal(in.want), "expect to match the extracted field name")
+	},
+		Entry("field has Names", fieldNameInput{
 			field: &ast.Field{
 				Names: []*ast.Ident{
 					{
@@ -23,16 +42,16 @@ func TestFieldName(t *testing.T) {
 				},
 			},
 			want: "foo",
-		},
-		"filed has no Names, but is an Ident": {
+		}),
+		Entry("field has no Names, but is an Ident", fieldNameInput{
 			field: &ast.Field{
 				Type: &ast.Ident{
 					Name: "foo",
 				},
 			},
 			want: "foo",
-		},
-		"field has no Names, but is a StarExpr with an Ident": {
+		}),
+		Entry("field has no Names, but is a StarExpr with an Ident", fieldNameInput{
 			field: &ast.Field{
 				Type: &ast.StarExpr{
 					X: &ast.Ident{
@@ -41,8 +60,8 @@ func TestFieldName(t *testing.T) {
 				},
 			},
 			want: "foo",
-		},
-		"field has no Names, and is not an Ident or StarExpr": {
+		}),
+		Entry("field has no Names, and is not an Ident or StarExpr", fieldNameInput{
 			field: &ast.Field{
 				Type: &ast.ArrayType{
 					Elt: &ast.Ident{
@@ -51,17 +70,6 @@ func TestFieldName(t *testing.T) {
 				},
 			},
 			want: "",
-		},
-	}
-
-	for name, tc := range cases {
-		t.Run(name, func(t *testing.T) {
-			t.Parallel()
-
-			got := utils.FieldName(tc.field)
-			if got != tc.want {
-				t.Errorf("got %q, want %q", got, tc.want)
-			}
-		})
-	}
-}
+		}),
+	)
+})
